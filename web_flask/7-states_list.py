16657817fca58186717c0c25873d0a81ce675da5
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-09-27 12:44:59 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
+"""
+starts a Flask web application
+"""
 
-from flask import Flask
-from flask import render_template
-
-
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
